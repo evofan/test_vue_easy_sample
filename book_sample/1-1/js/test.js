df8345c58a53053a30758462b33ecf7c28d1ea09
evofan <evofan@outlook.jp>
2020-10-09 20:21:56 +0900

    add file

---
+++ book_sample/1-1/js/test.js
@@ -0,0 +1,5 @@
+﻿console.log("test-");
+
+new Vue({
+    el: '#app'
+});
---
+++ book_sample/1-2/js/test.js
@@ -0,0 +1,10 @@
+﻿console.log("test-");
+
+const app = new Vue({
+    el: '#app',
+    data(){
+        return {
+            text :"hello, Vue.js"
+        };
+    }
+});

775931a91ff99a4d7cdd5e79cba11f11842ecc0c
evofan <evofan@outlook.jp>
2020-10-12 22:26:39 +0900

    add chap4 sample

---
+++ book_sample/4_1/js/test.js
@@ -0,0 +1,22 @@
+﻿console.log("test3-2");
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 0 // ★State ★Actionによって状態が変更される
+        };
+    },
+    methods: {
+        increment: function () { // ★Action
+            this.count++;
+        },
+        decrement() { // ★Action
+            this.count--;
+        }
+    }
+});
+
+Vue.config.devtools = true;
+
+// Vueの条件付きレンダリング...ある条件を満たす時だけレンダリングする

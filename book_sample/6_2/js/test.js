3f76f5e4023acf6d623455bbd1bfb96e603a99e6
evofan <evofan@outlook.jp>
2020-10-17 22:56:00 +0900

    add chap6 sample

---
+++ book_sample/6_2/js/test.js
@@ -0,0 +1,38 @@
+﻿console.log("test6-2 スタイルバインディング");
+
+const app = new Vue({
+    el: '#app',
+
+    // （1）文字列指定でのスタイルバインディング
+    data() {
+        return {
+            width: 100,
+            height: 100,
+            color: "#333"
+        };
+    },
+    computed: {
+        bindStyle() {
+            return `width: ${this.width}px; height: ${thid.height}px; color: ${this.color}; `;
+        }
+    },
+
+    // （2）オブジェクト指向でのスタイルバインディング
+    // ＝直接v-bind:styleで指定、その場合★スタイルの指定はキャメルケースで指定する
+    /*
+    data() {
+        return {
+            width: 100,
+            height: 100,
+            bgColor: "#fcfcfa"
+        };
+    },
+    ※HTML内で
+    <div : style = "{width: width +'px', height: hieght + 'px', backgroundColor: bgColor }"></div>
+    */
+});
+
+Vue.config.devtools = true;
+
+// ■スタイルバインディング=スタイルを動的に切り替える
+// v-bind:style="何らかのスタイル"　→　:style="何らかのスタイル"

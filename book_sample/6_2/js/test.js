bd403d7acf6683245e8608f862a942d10b0ff104
evofan <evofan@outlook.jp>
2020-10-18 03:05:24 +0900

    add chap6 sample

--- book_sample/6_2/js/test.js
+++ book_sample/6_2/js/test.js
@@ -1,38 +1,22 @@
-﻿console.log("test6-2 スタイルバインディング");
+﻿console.log("test6-2 色を変化させるスライダー");
 
 const app = new Vue({
     el: '#app',
-
-    // （1）文字列指定でのスタイルバインディング
     data() {
         return {
-            width: 100,
-            height: 100,
-            color: "#333"
+            range: 10,
+            red: 0,
+            green: 0,
+            blue: 0
         };
     },
     computed: {
         bindStyle() {
-            return `width: ${this.width}px; height: ${thid.height}px; color: ${this.color}; `;
+            return `width: ${this.range}px; height: ${this.range}px; background: rgb(${this.red}, ${this.green}, ${this.blue});`;
         }
     },
 
-    // （2）オブジェクト指向でのスタイルバインディング
-    // ＝直接v-bind:styleで指定、その場合★スタイルの指定はキャメルケースで指定する
-    /*
-    data() {
-        return {
-            width: 100,
-            height: 100,
-            bgColor: "#fcfcfa"
-        };
-    },
-    ※HTML内で
-    <div : style = "{width: width +'px', height: hieght + 'px', backgroundColor: bgColor }"></div>
-    */
 });
 
 Vue.config.devtools = true;
 
-// ■スタイルバインディング=スタイルを動的に切り替える
-// v-bind:style="何らかのスタイル"　→　:style="何らかのスタイル"

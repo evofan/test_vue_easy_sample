7483dab1c3ff9d4fff4a83fd7dc0165f744d21d6
evofan <evofan@outlook.jp>
2020-10-19 01:05:09 +0900

    add chap6 sample

---
+++ book_sample/6_3/js/test.js
@@ -0,0 +1,30 @@
+﻿console.log("test6-3 クラスバインディング");
+
+const app = new Vue({
+    el: '#app',
+    /*
+    data() {
+        return {
+            isActive: true // trueの場合、HTML側で設定したclassが付与される
+        };
+    }
+    */
+
+    data() {
+        return {
+            classObj: {
+                error: false,
+                'is-active': false // errorとisActiveがtrueになると、"error is-active"というクラスが適用される
+                // ※複数のクラスをまとめて指定できる
+            }
+        };
+    }
+});
+
+Vue.config.devtools = true;
+
+// ■クラスバインディング＝class属性にデータをバインディングする
+// ・スタイル情報がインラインでなくCSS上に書けるので可読性が上がる
+// ・ハンバーガーボタンやモーダルのようなUIを実現出来る
+// <div v-bind:class="{'is-active': isActive}"></div>
+// <div :class="{'is-active': isActive}"></div>※短縮形
---
+++ book_sample/6_4/js/test.js
@@ -0,0 +1,21 @@
+﻿console.log("test6-4 ハンバーガーボタン");
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            isActive: false
+        };
+    },
+    methods: {
+        toggleButton() {
+            this.isActive = !this.isActive;
+        }
+    }
+});
+
+Vue.config.devtools = true;
+
+
+
+

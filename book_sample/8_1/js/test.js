046115956e7d9f0c32429c4c3d7ca21959cfa5fc
evofan <evofan@outlook.jp>
2020-10-29 07:31:05 +0900

    fix

--- book_sample/8_1/js/test.js
+++ book_sample/8_1/js/test.js
@@ -1,18 +1,23 @@
 ﻿console.log("test8-1 ビューワーアプリ");
 
-const app = new Vue({
-    el: '#app',
+// コンポーネントの書き方
+const anyComponent = Vue.component("any-component", { // ★コンポーネントはケバブベースで命名するのが普通
     data() {
         return {
-
-        };
+            title: "Hello, Component"
+        }
     },
-    methods: {
+    template: '<div class="title-label">{{ title }}</div>' // ★Vueインスタンスの時は無かったので注意
+});
 
-    },
-    computed: {
+// test.js:7 Uncaught ReferenceError: Cannot access 'anyComponent' before initialization
+// なのでコンポーネントより後に書く
+const app = new Vue({
+    el: '#app',
 
-    },
+    component: {
+        'any-component': anyComponent
+    }
 });
 
 Vue.config.devtools = true;
@@ -29,3 +34,26 @@ Vue.config.devtools = true;
 // ・ルートコンポーネント（全てのコンポーネントを集約する部分）
 // 　　L ・サムネールコンポーネント
 // 　　L ・モーダルコンポーネント
+
+// propsの書き方 ... 親コンポーネントが、子コンポーネントにデータを渡す
+/*
+props : {
+    プロパティ名: {
+        ルールの指定
+    }
+}
+*/
+
+/*
+props: {
+    name: {
+        type: String, // Strign以外の型の場合警告が出る
+        default: "Taro"
+    }
+}
+*/
+
+// emitの書き方 ... 子コンポーネントが親コンポーネントにイベントを通知する
+/*
+$emit(イベント名, [親コンポーネントに渡すデータ（あれば）]);
+*/
